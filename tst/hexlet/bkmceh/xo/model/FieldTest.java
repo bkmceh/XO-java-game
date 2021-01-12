389ae5743338ef1d7ef7ebada901677c1f9a39c6
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-01-13 01:19:26 +0300

    FieldTest checked exceptions

--- tst/hexlet/bkmceh/xo/model/FieldTest.java
+++ tst/hexlet/bkmceh/xo/model/FieldTest.java
@@ -83,4 +83,18 @@ class FieldTest {
             fail();
         } catch (final InvalidPointException e) {}
     }
+
+    @Test
+    void setFigureWhenAlreadyOccupied() throws AlreadyOccupiedException, InvalidPointException {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
+    }
 }
