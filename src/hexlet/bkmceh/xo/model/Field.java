1e84d8e912385c3101ff02f9fedc674e0c434273
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-01-16 20:15:14 +0300

    Field - made size more flexible

--- src/hexlet/bkmceh/xo/model/Field.java
+++ src/hexlet/bkmceh/xo/model/Field.java
@@ -1,22 +1,24 @@
 package hexlet.bkmceh.xo.model;
 
-import hexlet.bkmceh.xo.model.exceptions.AlreadyOccupiedException;
 import hexlet.bkmceh.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
+    private final int fieldSize;
 
     private static final int MIN_COORDINATE = 0;
 
-    private static final int MAX_COORDINATE = FIELD_SIZE;
+    private final Figure[][] field;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        this.field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
@@ -38,6 +40,6 @@ public class Field {
     }
 
     private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+        return coordinate >= MIN_COORDINATE && coordinate < fieldSize;
     }
 }
