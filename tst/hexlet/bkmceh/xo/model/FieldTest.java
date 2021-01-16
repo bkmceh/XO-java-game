085144a812b731fcbfe33d8e895a7dcf212d96d5
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-01-16 19:35:19 +0300

    FieldTest - remove useless test

--- tst/hexlet/bkmceh/xo/model/FieldTest.java
+++ tst/hexlet/bkmceh/xo/model/FieldTest.java
@@ -84,17 +84,4 @@ class FieldTest {
         } catch (final InvalidPointException e) {}
     }
 
-    @Test
-    void setFigureWhenAlreadyOccupied() throws AlreadyOccupiedException, InvalidPointException {
-        final Field field = new Field();
-        final Point inputPoint = new Point(0, 0);
-        final Figure inputFigure = Figure.O;
-
-        field.setFigure(inputPoint, inputFigure);
-
-        try {
-            field.setFigure(inputPoint, inputFigure);
-            fail();
-        } catch (final AlreadyOccupiedException e) {}
-    }
 }
